6f292cd6360ea396a0eaed5a55237bb29669b090
Jonathan Staats <jjstaats@gmail.com>
2015-03-22 03:10:04 +0100

    Created message

---
+++ ProjectOstrich/Data/Message.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ProjectOstrich
+{
+	public class Message
+	{
+		public Message ()
+		{
+		}
+
+		/// <summary>
+		/// Phone number hash.
+		/// </summary>
+		/// <value>The identifier.</value>
+		public string Identifier { get; set; }
+
+		public string Data { get; set; }
+
+	}
+}
+
