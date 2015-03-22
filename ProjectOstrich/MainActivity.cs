436629467df7235dc9a1c44c8700a03b52d049cc
Jakob Buis <jpabuis@gmail.com>
2015-03-22 04:42:10 +0100

    added dummy list view

--- ProjectOstrich/MainActivity.cs
+++ ProjectOstrich/MainActivity.cs
@@ -10,16 +10,25 @@ using Android.OS;
 namespace ProjectOstrich
 {
 	[Activity (Label = "ProjectOstrich", MainLauncher = true, Icon = "@drawable/ostrich")]
-	public class MainActivity : Activity
+	public class MainActivity : ListActivity
 	{
+		string[] items;
 		int count = 1;
 		private ExchangeManager _manager;
 
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
+			items = new string[] { "Vegetables","Fruits","Flower Buds","Legumes","Bulbs","Tubers" };
+			ListAdapter = new ArrayAdapter<String>(this, Android.Resource.Layout.SimpleListItem1, items);
 			SetContentView (Resource.Layout.Main);
 			_manager = new ExchangeManager (BaseContext, this);
+		}
+
+		protected override void OnListItemClick(ListView l, View v, int position, long id)
+		{
+			var t = items[position];
+			Android.Widget.Toast.MakeText(this, t, Android.Widget.ToastLength.Short).Show();
 		}
 
 		public override bool OnCreateOptionsMenu (IMenu menu)
